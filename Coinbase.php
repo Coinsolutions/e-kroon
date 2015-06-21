1398c8c81ad7b38687287396dadcb47d92bad449
Wisewords615 <randma.martti@gmail.com>
2015-06-21 18:21:19 +0300

    Update Coinbase.php
    First transacript

--- Coinbase.php
+++ Coinbase.php
@@ -1,2 +1,14 @@
-This will be the coinbasde server code.
-Analysing the request taken by pools, making transactions, holding wallets aka prefixes.
+<?php 
+start_session();
+if($_SESSION['rock']= martti){
+$file = './coinbase.txt';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+$current .= $_SESSION['rock']."\n";
+// Write the contents back to the file
+file_put_contents($file, $current);
+}
+
+
+?>
