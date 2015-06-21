25136db8487d9338b6fa682c8ca227226b67bb3b
Wisewords615 <randma.martti@gmail.com>
2015-06-21 21:52:49 +0300

    Update Rock.php
    Cross-domain rock for miner.js, miner is mining rock's linecount on a text file.

--- Rock.php
+++ Rock.php
@@ -1,16 +1,17 @@
 <?php
+If(!$_SESSION){
 session_start();
- header('Access-Control-Allow-Origin: *');
 $aeg = $_GET['aeg'];
 $prefix = $_GET['prefix'];
 $ip = $_GET['ip'];
-$_SESSION['mine_60'] = $_SESSION['mine_60'] +1;
+$_SESSION['mine_60'] = $_SESSIO['mine_60']+1;
+}
 if($_SESSION['mine_60'] = 60){
 $file = './rock.txt';
 // Open the file to get existing content
 $current = file_get_contents($file);
 // Append a new person to the file
-$current .= $aeg."; miner :".$prefix.";ip:".$ip.";\n";
+$current .= $prefix."\n";
 // Write the contents back to the file
 file_put_contents($file, $current);
 }
@@ -23,15 +24,18 @@ while(!feof($handle)){
 }
 
 fclose($handle);
-
-If($linecount = 60){
-$url="http://twojointsontheroad.info/e-kroon/coinbase.php?mine=60&rock=martti";
+If(!$_SESSION['linecount']){
+$url="http://twojointsontheroad.info/e-
+kroon/coinbase.php?mine=".$linecount."&rock=".$prefix;
 $ch = curl_init();
-curl_setopt( $ch, CURLOPT_USERAGENT, "Im_Rock" );
+curl_setopt( $ch, CURLOPT_USERAGENT, 
+"Im_Rock" );
 curl_setopt( $ch, CURLOPT_URL, $url );  
 $content = curl_exec( $ch );
 $response = curl_getinfo( $ch );
 curl_close ( $ch );
 }
+$_SESSION['linecount'] = $linecount;
+
 
 ?>
