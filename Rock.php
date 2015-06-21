d0342fd167cc5aa580c9785e2322c5da4f987c0e
Wisewords615 <randma.martti@gmail.com>
2015-06-21 18:41:45 +0300

    Update Rock.php

--- Rock.php
+++ Rock.php
@@ -1,9 +1,11 @@
 <?php
+session_start();
  header('Access-Control-Allow-Origin: *');
 $aeg = $_GET['aeg'];
 $prefix = $_GET['prefix'];
 $ip = $_GET['ip'];
-
+$_SESSION['mine_60'] = $_SESSION['mine_60'] +1;
+if($_SESSION['mine_60'] = 60){
 $file = './rock.txt';
 // Open the file to get existing content
 $current = file_get_contents($file);
@@ -11,5 +13,25 @@ $current = file_get_contents($file);
 $current .= $aeg."; miner :".$prefix.";ip:".$ip.";\n";
 // Write the contents back to the file
 file_put_contents($file, $current);
+}
+$file="rock.txt";
+$linecount = 0;
+$handle = fopen($file, "r");
+while(!feof($handle)){
+  $line = fgets($handle);
+  $linecount++;
+}
+
+fclose($handle);
+
+If($linecount = 60){
+$url="http://twojointsontheroad.info/e-kroon/coinbase.php?mine=60&rock=martti";
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_USERAGENT, "Im_Rock" );
+curl_setopt( $ch, CURLOPT_URL, $url );  
+$content = curl_exec( $ch );
+$response = curl_getinfo( $ch );
+curl_close ( $ch );
+}
 
 ?>
